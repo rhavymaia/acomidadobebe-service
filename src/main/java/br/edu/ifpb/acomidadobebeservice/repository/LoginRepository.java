4aa2cdcf080353b0c5454a72b0cb874be937c239
CleoniceSoares <cleonice.soares27cs@gmail.com>
2022-05-13 16:05:04 -0300

    Atualizando o login

--- src/main/java/br/edu/ifpb/acomidadobebeservice/repository/LoginRepository.java
+++ src/main/java/br/edu/ifpb/acomidadobebeservice/repository/LoginRepository.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.acomidadobebeservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.edu.ifpb.acomidadobebeservice.model.Login;
 
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Integer> {
+
+    Optional<Login> findByKey(String key);
     
 }
